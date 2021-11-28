8d9192f30e4e59be6716afe5dec1c6045a6ed5b9
KS7262 <kolya1999starodub@gmail.com>
2021-11-28 09:07:33 +0200

    second commit for lb7.3

---
+++ LB7.3/Article.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LB7._3
+{
+    class Article
+    {
+        private string product;
+        private string store;
+        private double price;
+
+        public Article(string product, string store, double price)
+        {
+            this.product = product;
+            this.store = store;
+            this.price = price;
+        }
+        public string Article2
+        {
+            get { return product; }
+            set { product = value; }
+        }
+        public string Print()
+        {
+            return $"Название товара - {product}\nЦена - {price}\nМагазин - {store}";
+        }
+    }
+}
---
+++ LB7.3/Store.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LB7._3
+{
+    class Store
+    {
+        Article[] article = new Article[3];
+
+        public Store()
+        {
+            article[0] = new Article("телефон", "Эльдорадо", 7500);
+            article[1] = new Article("ноутбук", "Цитрус", 22000);
+            article[2] = new Article("телевизор", "Фокстрот", 15000);
+        }
+        public string this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index < article.Length)
+                {
+                    return article[index].Print();
+                }
+                else
+                    return "Пока что столько товаров у нас нету.";
+            }
+
+        }
+
+        public string this[string index]
+        {
+            get
+            {
+                for (int i = 0; i < article.Length; i++)
+                {
+                    if (article[i].Article2 == index)
+                    {
+                        return article[i].Print();
+                    }
+
+                }
+                return $"{index} - пока что такого товара нету";
+            }
+        }
+
+    }
+}
